05eed655699ed127e1916d009e31d4c6782059f5
dreamlx <dreamlinx@gmail.com>
2014-03-01 15:51:52 +0800

    fix callback path

--- app/controllers/api/v1/media_controller.rb
+++ app/controllers/api/v1/media_controller.rb
@@ -3,4 +3,10 @@ class Api::V1::MediaController < Api::BaseController
     instagrams = Instagram.media_popular
     render json: instagrams.to_json
   end
+
+  def tag_search
+    # tag_search
+    instagrams = Instagram.media_tag_search
+    render json: instagrams.to_json
+  end
 end
