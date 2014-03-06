32251e0e2c1022dae8649a2b355bb5b05885e59c
dreamlx <dreamlinx@gmail.com>
2014-03-06 16:19:11 +0800

    added token

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,20 @@
 class UsersController < ApplicationController
   #before_filter :authenticate_user!
-  TOKEN = session[:access_token]
+  
   #TOKEN = '789981918.80d957c.f408bb1006844250907d9d2a34df66c4'
   def index
     @users = User.all
   end
 
   def profile
+    TOKEN = session[:access_token]
     client = Instagram.client(:access_token => TOKEN)
     user = client.user
     render json: user.to_json
   end
 
   def feed
+    TOKEN = session[:access_token]
     client = Instagram.client(:access_token => TOKEN)
     @medias = client.user_recent_media(:count => 20)
     @user = client.user
