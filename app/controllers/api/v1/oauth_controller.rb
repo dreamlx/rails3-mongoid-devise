e7909ce32786b5680709a02429911756075ef9c0
dreamlx <dreamlinx@gmail.com>
2014-03-06 10:55:26 +0800

    fix callback

--- app/controllers/api/v1/oauth_controller.rb
+++ app/controllers/api/v1/oauth_controller.rb
@@ -1,16 +1,14 @@
 class Api::V1::OauthController < Api::BaseController
+  CALLBACK = "http://test.videoshowapp.com:8087/api/v1/oauth/callback"
   def index
   end
 
   def connect
-    callback_url = "http://128.199.224.228/api/v1/oauth/callback"
-
-    redirect_to Instagram.authorize_url(:redirect_uri => callback_url)
+    redirect_to Instagram.authorize_url(:redirect_uri => CALLBACK)
   end
 
   def callback
-    callback_url = "http://128.199.224.228/api/v1/oauth/callback"
-    response = Instagram.get_access_token(params[:code], :redirect_uri => callback_url)
+    response = Instagram.get_access_token(params[:code], :redirect_uri => CALLBACK)
     session[:access_token] = response.access_token
     redirect_to api_v1_users_feed_path
   end
